fa56fda5d7962207fc058422a56093631b140437
dev-ritik <ritikkne@gmail.com>
2018-12-08 00:04:48 +0530

    static_files added

--- politoons/settings.py
+++ politoons/settings.py
@@ -115,10 +115,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = '/myapp/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'myapp/static'),
 )
 django_heroku.settings(locals())
